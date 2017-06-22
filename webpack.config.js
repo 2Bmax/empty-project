4751ae241af3bfc1e3ef6ccb261e75760a92c696
2BMax <zaikinma@gmail.com>
2017-06-22 18:23:07 +0300

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,37 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-	entry:{
-		index:'./src/index.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
-	},
-	module:{
-		rules:[
-			{
-				test:/\.js$/,
-				include: [
-					path.resolve(__dirname, "src")
-		        ],
-			}
-		]
+console.log('============================= '+process.env.NODE_ENV+' BUILD =============================');
+
+module.exports = function(){
+	return{
+		entry:{
+			index:'./src/index.js'
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].bundle.js'
+		},
+	 	context: __dirname,
+		module:{
+			rules:[
+				{
+					test:/\.js$/,
+					include: [
+						path.resolve(__dirname, 'src')
+			        ],
+					exclude: [
+	 					path.resolve(__dirname, 'node_modules'),
+						path.resolve(__dirname, 'dist')
+			 		],
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015','react']
+						}
+					}
+				}
+			]
+		}
 	}
 };
